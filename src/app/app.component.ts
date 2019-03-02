7fabdd02189fc300cd333facc12aa91c90d86751
susehan <susannahan27@gmail.com>
2019-03-02 16:11:29 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
-  title = 'ng7';
+  title = 'My First Angular App';
 }
+
